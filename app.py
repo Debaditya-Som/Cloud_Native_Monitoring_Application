9ee3e188c4a060286c12517e8a3eb09e9413dc2f
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-07-22 23:00:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import psutil 
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    cpu_percent = psutil.cpu_percent()
+    mem_percent = psutil.virtual_memory().percent
+    message = None
+    if cpu_percent>80 or mem_percent>80:
+        message="High CPU or Memory Utilization detected. Please scle up"
+    return f"CPU Utilization:{cpu_percent} and Memory Utilization:{mem_percent}"
+
+if __name__ == '__main__':
+    app.run(debug= True, host='0.0.0.0')     
